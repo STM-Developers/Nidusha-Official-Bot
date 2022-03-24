a7ee42a798a7d1f903f12284cb2fdff7071bb8e1
Nidusha Amarasinghe <95688464+NidushaAmarasinghe@users.noreply.github.com>
2022-03-24 22:27:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,44 +21,53 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-
-from cgitb import text
 import telebot, requests, json
 from telebot import types
 from os import getenv
 
 bot = telebot.TeleBot(getenv("BOT_TOKEN"))
 
-# /help command menu
-help = f"""
-how are you? sen your problems to [🔰here🔰](https://t.me/STM_Developers)
-"""
 
 # Markup
 mark1 = telebot.types.InlineKeyboardMarkup()
-mark1.add(telebot.types.InlineKeyboardButton(text='🔁Updates🔁', url='https://t.me/STMDevelopers'),
-          telebot.types.InlineKeyboardButton(text='🧑‍💻Support🧑‍💻', url='https://t.me/STM_Developers')),
-mark1.add(telebot.types.InlineKeyboardButton(text='🔰Github🔰', url='https://github.com/NidushaAmarasinghe')),
+mark1.add(telebot.types.InlineKeyboardButton(text='🔁Updates🔁', url='https://t.me/SlapTap'),
+          telebot.types.InlineKeyboardButton(text='🧑‍💻Support🧑‍💻', url='https://t.me/SlapTaps')),
+mark1.add(telebot.types.InlineKeyboardButton(text='➕Add To Group➕', url="https://t.me/NidushaOfficial_Bot?startgroup=true")),
 
 mark2 = telebot.types.InlineKeyboardMarkup()
-mark2.add(telebot.types.InlineKeyboardButton(text='🛠️Developer🛠️', url="http://t.me/NidushaAmarasinghe"),
-          telebot.types.InlineKeyboardButton(text='🔰Supoort🔰', url='https://t.me/STM_Developers')),
-mark2.add(telebot.types.InlineKeyboardButton(text='❔Help❔', url="https://t.me/STMDevelopers")),
-# Commands
-@bot.message_handler(commands=['start'])
-def send_start(message):
-   bot.send_message(message.chat.id, text="💕Hi There! 😁Welcome To Nidusha Official Bot😘\n◦•●◉✿ [Powerd By](https://t.me/STM_Developers) ✿◉●•◦",parse_mode='Markdown', reply_markup=mark1)
+mark2.add(telebot.types.InlineKeyboardButton(text='🔁Updates🔁', url='https://t.me/SlapTap'),
+          telebot.types.InlineKeyboardButton(text='🧑‍💻Support🧑‍💻', url='https://t.me/SlapTaps')),
+mark2.add(telebot.types.InlineKeyboardButton(text='🛠️Developer🛠️', url="https://t.me/NidushaAmarasinghe")),
 
+mark3 = telebot.types.InlineKeyboardMarkup()
+mark3.add(telebot.types.InlineKeyboardButton(text='🔁Updates🔁', url='https://t.me/SlapTap'),
+          telebot.types.InlineKeyboardButton(text='🧑‍💻Support🧑‍💻', url='https://t.me/SlapTaps')),
+mark3.add(telebot.types.InlineKeyboardButton('❔About❔',query.answer("Hi ",show_alert=true)),
+
+
+
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
+  bot.reply_to(message, "**Hey There! This Is @NidushaAmarasinghe's Official Bot\nJoin:-@SlapTap**",parse_mode='Markdown', reply_markup=mark1)
+   
 @bot.message_handler(commands=["help"])
-def send_help(message):
-    bot.send_message(message.chat.id, text="how are you? sent your problems to [🔰here🔰](https://t.me/STM_Developers)",parse_mode='Markdown', reply_markup=mark2) 
-
-@bot.message_handler(commands=["about"])
-def send_about(message):
-    bot.send_message(message.chat.id, text="This Is Nidusha Amarasinghe's Official Bot!\nDeverloper-@NidushaAmarasinghe",parse_mode='Markdown', reply_markup=mark2)
-
+def send_message(message):
+  bot.send_message(message.chat.id, "Do You Want Help❔",parse_mode='Markdown', reply_markup=mark2)
+ 
 @bot.message_handler(commands=["alive"])
-def send_help(message):
-    bot.send_message(message.chat.id, text="Hey There! Bot Online now. 💃🏻\n♥️Developer: ɳιԃυαԋα αɱαɾαʂιɳɠԋҽ\nSupport: @SlapTaps\nThank You For Using Niduha Official Bot💞") 
+def send_welcome(message):
+  bot.reply_to(message, "Hey There! Bot Online now. 💃🏻♥️\nDeveloper:-@NidushaAmarasinghe\nOfficial Website:-www.nidushaofficial.tk\nThank You For Using Nidusha Official Bot")
+ 
+@bot.message_handler(commands=["about"])
+def send_welcome(message):
+  bot.reply_to(message, "Hey There! This Is @NidushaAmarasinghe's Official Bot\nJoin:-@SlapTap",parse_mode='Markdown', reply_markup=mark3)
+  
+@bot.message_handler(content_types=["photo", "sticker"])
+def send_content_message(msg):
+    bot.reply_to(msg, "That's not a text message!")
+    
+
+    
+
 
 bot.polling()
